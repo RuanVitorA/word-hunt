1da4fad08fe2248202e4ff0d7c680efa83e41d4f
ruan vitor <ruanvitorandrade2001@gmail.com>
2022-09-02 15:00:37 -0400

    first commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <hr style={{ width: "90%", marginTop: 20 }} />
+      <span className="name">
+        Made by{" "}
+        <a href="https://www.linkedin.com/in/ruan-vitor-andrade-5b289120b/" target="__blank">
+          Moose
+        </a>
+      </span>
+      </div>
+  );
+};
+
+export default Footer;
---
+++ src/data/category.js
@@ -0,0 +1,6 @@
+const categories = [
+    { value: "Português", label: "pt-BR" },
+    { value: "Spanish", label: "es" },
+    { value: "English", label: "en" },
+  ]; 
+  export default categories;
